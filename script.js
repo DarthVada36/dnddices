728de0f383ddeb9e5fc1f25bd11bb1bec74f4b6e
DarthVada36 <velazquez.vada@outlook.com>
2024-08-29 15:07:19 +0200

    First Commit const added

---
+++ script.js
@@ -0,0 +1,17 @@
+// Math.trunc - Recorta el nùmero sin importar la distancia//
+
+const randomm = Math.trunc(Math.random() * 6) + 1;
+console.log(randomm);
+
+// Math.ceil- siempre redondea hacia arriba//
+const numero = Math.ceil(Math.random () * 6);
+console.log(numero);
+
+//Math.floor- redondea numeros hacia abajo//
+const number = Math.floor(Math.random() * 6) + 1;
+console.log(number);
+
+//Math.round - redondea numeros al mas cercano (arriba o abajo)//
+const numberr = Math.round(Math.random() * 5) + 1;
+console.log(numberr);
+
